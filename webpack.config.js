b33be504924e0b10dfc5924a1ab2132d8ec87f70
matheus <matheus.xmaz10@gmail.com>
2022-02-18 23:08:19 -0300

    importando arquivos css

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,11 @@ module.exports = {
         exclude: /node_modules/,
         use: "babel-loader",
       },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
 };
