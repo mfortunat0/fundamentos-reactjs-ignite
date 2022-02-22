e85283c8016a20ec0af0dc4d63d3ae944aab0e67
matheus <matheus.xmaz10@gmail.com>
2022-02-22 14:30:42 -0300

    primeiro componente react

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,31 @@
+const repositoryName = "unform";
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositorios</h1>
+      <ul>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in react</p>
+          <a href="">Acessar repositorio</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+          <a href="">Acessar repositorio</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+          <a href="">Acessar repositorio</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+          <a href="">Acessar repositorio</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
